10f756f2bfdc3cff21859246b466c20f91121891
noikiy <noikiy@live.com>
2019-09-27 11:32:05 +0800

    [超级赠品]数据文件

--- develop/gift_plus/migrate.sql
+++ develop/gift_plus/migrate.sql
@@ -32,12 +32,12 @@ ALTER TABLE `ims_ewei_shop_order` ADD `is_gift_plus` tinyint(3) NOT NULL DEFAULT
 -- 2019-09-25 15:33:14
 ALTER TABLE `ims_ewei_shop_order_goods` ADD `gift_price` DECIMAL(10,2) NULL DEFAULT '0.00' COMMENT '赠品结算价' AFTER `is_gift_plus`;
 ALTER TABLE `ims_ewei_shop_order_goods` ADD `gift_price_cost` DECIMAL(10,2) NULL DEFAULT '0.00' COMMENT '赠品成本价' AFTER `gift_price`;
-ALTER TABLE `ims_ewei_shop_order_goods` ADD `gift_price_market` DECIMAL(10,2) NULL DEFAULT '0.00' COMMENT '赠品成本价' AFTER `gift_price_cost`;
+ALTER TABLE `ims_ewei_shop_order_goods` ADD `gift_price_market` DECIMAL(10,2) NULL DEFAULT '0.00' COMMENT '赠品对应的市场价' AFTER `gift_price_cost`;
 
 -- 2019-09-26 12:57:15
 ALTER TABLE `ims_ewei_shop_order` ADD `gift_plus_price` DECIMAL(10,2) NULL DEFAULT '0.00' COMMENT '赠品结算价' AFTER `is_gift_plus`;
 ALTER TABLE `ims_ewei_shop_order` ADD `gift_plus_cost` DECIMAL(10,2) NULL DEFAULT '0.00' COMMENT '赠品成本价' AFTER `gift_plus_price`;
-ALTER TABLE `ims_ewei_shop_order` ADD `gift_plus_market` DECIMAL(10,2) NULL DEFAULT '0.00' COMMENT '赠品成本价' AFTER `gift_plus_cost`;
+ALTER TABLE `ims_ewei_shop_order` ADD `gift_plus_market` DECIMAL(10,2) NULL DEFAULT '0.00' COMMENT '赠品对应的市场价' AFTER `gift_plus_cost`;
 
 -- 2019-09-26 15:11:35
 ALTER TABLE `ims_ewei_shop_order_goods` ADD `gift_plus_merchid` int(11) NOT NULL DEFAULT 0 COMMENT '赠品使用商户' AFTER `gift_price_market`;
